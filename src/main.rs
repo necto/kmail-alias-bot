af85537bea0903bbd7e5f5a9ea319fbad0469541
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2024-11-26 04:43:38 +0100

    Use dialogues for data entry

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,177 @@
-use teloxide::{prelude::*, utils::command::BotCommands};
+use teloxide::{
+    dispatching::{dialogue, dialogue::InMemStorage, UpdateHandler},
+    prelude::*,
+    utils::command::BotCommands,
+};
+
+type MyDialogue = Dialogue<State, InMemStorage<State>>;
+type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
+
+#[derive(Clone, Default)]
+pub enum State {
+    #[default]
+    Start,
+    ReceiveNewAliasName,
+    ReceiveNewAliasDescription {
+        alias_name: String,
+    },
+
+    ReceiveAliasNameForRemoval,
+}
+
+/// These commands are supported:
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase")]
+enum Command {
+    /// Display this text.
+    Help,
+    /// Start adding a new alias.
+    Add,
+    /// Cancel adding a new alias.
+    Cancel,
+    /// List all aliases.
+    ListAliases,
+    /// Remove an alias.
+    Remove,
+}
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    log::info!("Starting command bot...");
+    log::info!("Starting kMail alias bot...");
 
     let bot = Bot::from_env();
 
-    Command::repl(bot, answer).await;
+    Dispatcher::builder(bot, schema())
+        .dependencies(dptree::deps![InMemStorage::<State>::new()])
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
 }
 
-#[derive(BotCommands, Clone)]
-#[command(rename_rule = "lowercase", description = "These commands are supported:")]
-enum Command {
-    #[command(description = "Display this text.")]
-    Help,
-    #[command(description = "Add an alias.", parse_with = "split")]
-    Alias(String, String),
-    #[command(description = "List aliases.")]
-    List,
-    #[command(description = "Remove an alias.")]
-    RemoveAlias(String),
+fn schema() -> UpdateHandler<Box<dyn std::error::Error + Send + Sync + 'static>> {
+    use dptree::case;
+
+    let command_handler = teloxide::filter_command::<Command, _>()
+        .branch(
+            case![State::Start]
+                .branch(case![Command::Help].endpoint(help))
+                .branch(case![Command::Add].endpoint(start_new_alias))
+                .branch(case![Command::ListAliases].endpoint(list_aliases))
+                .branch(case![Command::Remove].endpoint(start_removing_alias)),
+        )
+        .branch(case![Command::Cancel].endpoint(cancel));
+
+    let message_handler = Update::filter_message()
+        .branch(command_handler)
+        .branch(case![State::ReceiveNewAliasName].endpoint(receive_new_alias_name))
+        .branch(case![State::ReceiveNewAliasDescription { alias_name  }].endpoint(receive_alias_description))
+        .branch(case![State::ReceiveAliasNameForRemoval].endpoint(receive_alias_name_for_removal))
+        .branch(dptree::endpoint(invalid_state));
+
+    dialogue::enter::<Update, InMemStorage<State>, State, _>()
+        .branch(message_handler)
 }
 
-async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    match cmd {
-        // TODO: Display the arguments for the commands
-        Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
-        Command::Alias(alias, description) => {
-            // TODO: Implement a dialog to enable verbose descriptions
-            bot.send_message(msg.chat.id, format!("Requesting alias {alias}@example.com for {description}")).await?;
-            bot.send_message(msg.chat.id, format!("Command not implemented")).await?
+async fn start_new_alias(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    bot.send_message(msg.chat.id, "Enter the single-word name of the alias to add").await?;
+    dialogue.update(State::ReceiveNewAliasName).await?;
+    Ok(())
+}
+
+async fn help(bot: Bot, msg: Message) -> HandlerResult {
+    bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?;
+    Ok(())
+}
+
+async fn list_aliases(bot: Bot, msg: Message) -> HandlerResult {
+    bot.send_message(msg.chat.id, "Listing aliases is not implemented").await?;
+    Ok(())
+}
+
+async fn start_removing_alias(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    // TODO: list all aliases here and let them choose
+    // Here is an example of a selection:
+    // https://github.com/teloxide/teloxide/blob/master/crates/teloxide/examples/purchase.rs
+    bot.send_message(msg.chat.id, "Enter the single-word name of the alias to remove").await?;
+    dialogue.update(State::ReceiveAliasNameForRemoval).await?;
+    Ok(())
+}
+
+async fn cancel(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    bot.send_message(msg.chat.id, "Cancelling the dialogue.").await?;
+    dialogue.exit().await?;
+    Ok(())
+}
+
+async fn invalid_state(bot: Bot, msg: Message) -> HandlerResult {
+    bot.send_message(msg.chat.id, "Unable to handle the message. Type /help to see the usage.")
+       .await?;
+    Ok(())
+}
+
+async fn receive_alias_name_for_removal(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    match msg.text().map(ToOwned::to_owned) {
+        Some(alias_name) => {
+            // TODO: validation: matches one of the existing aliases
+            bot.send_message(msg.chat.id, format!("Removing alias {alias_name}@example.com")).await?;
+            bot.send_message(msg.chat.id, "Not implemented").await?;
+            dialogue.exit().await?;
         }
-        Command::List => {
-            bot.send_message(msg.chat.id, format!("Listing all aliases @example.com")).await?;
-            bot.send_message(msg.chat.id, format!("Not implemented")).await?
+        None => {
+            bot.send_message(msg.chat.id, "Please, send me a single-word alias name.").await?;
         }
-        Command::RemoveAlias(alias) => {
-            bot.send_message(msg.chat.id, format!("Requesting to remove alias {alias}@example.com")).await?;
-            bot.send_message(msg.chat.id, format!("Command not implemented")).await?
+    }
+
+    Ok(())
+}
+
+
+async fn receive_new_alias_name(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    match msg.text().map(ToOwned::to_owned) {
+        Some(alias_name) => {
+            // TODO: validation: single word, no '@', etc.
+            bot.send_message(msg.chat.id, "Enter the description of the alias").await?;
+            dialogue.update(State::ReceiveNewAliasDescription { alias_name }).await?;
         }
-    };
+        None => {
+            bot.send_message(msg.chat.id, "Please, send me a single-word alias name.").await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn receive_alias_description(
+    bot: Bot,
+    dialogue: MyDialogue,
+    alias_name: String, // Available from `State::ReceiveAliasDescription`.
+    msg: Message,
+) -> HandlerResult {
+    match msg.text().map(ToOwned::to_owned) {
+        Some(description) => {
+            bot.send_message(
+                dialogue.chat_id(),
+                format!("Adding alias {alias_name}@example.com. With description:"),
+            )
+               .await?;
+            bot.send_message(
+                dialogue.chat_id(),
+                description,
+            )
+               .await?;
+            bot.send_message(
+                dialogue.chat_id(),
+                "Not implemented.",
+            )
+               .await?;
+            dialogue.exit().await?;
+        }
+        None => {
+            bot.send_message(msg.chat.id, "Please, send me your full name.").await?;
+        }
+    }
 
     Ok(())
 }
