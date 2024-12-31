d36b394dba6d6d03c3d7c841b01a5d3a3466c391
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2024-12-31 20:40:10 +0100

    Test the auth token in the mock

--- src/test.rs
+++ src/test.rs
@@ -1,13 +1,12 @@
 use super::*;
 use mockito::Server;
 use teloxide_tests::{MockBot, MockMessageText};
-use tokio::sync::Mutex;
 
 fn mock_config() -> Config {
     Config {
-        teloxide_token: "mock_teloxide_token1234".to_string(),
+        teloxide_token: "123teloxide_api_token".to_string(),
 
-        kmail_token: "mock_kmail_token1234".to_string(),
+        kmail_token: "123mock_kmail_token".to_string(),
         mail_hosting_id: "mock_mail_hosting_id".to_string(),
         mailbox_name: "mock_name".to_string(),
 
@@ -52,10 +51,11 @@ async fn test_help_msg() {
 // TODO: find out why the doc describe a different shape of the response
 // https://developer.infomaniak.com/docs/api/get/1/mail_hostings/%7Bmail_hosting_id%7D/mailboxes/%7Bmailbox_name%7D/aliases
 #[tokio::test]
-async fn test_list_aliases() {
+async fn test_api_list_aliases() {
     let mut server = Server::new_async().await;
-    let mock = server.mock("GET", "/1/mail_hostings/mail_id/mailboxes/mailbox_name/aliases")
-                        .with_body(r#"
+    let mock = server.mock("GET", "/1/mail_hostings/mock_mail_hosting_id/mailboxes/mock_name/aliases")
+                     .match_header(reqwest::header::AUTHORIZATION, "Bearer 123mock_kmail_token")
+                     .with_body(r#"
 
 {
 "result":"success",
@@ -69,9 +69,9 @@ async fn test_list_aliases() {
 "#)
                         .create_async()
                         .await;
-    // TODO: Check that auth token is provided in the request
 
-    let api = KMailApi::new("token", "mail_id", "mailbox_name", &server.url());
+    let config = mock_config();
+    let api = Arc::new(KMailApi::new(&config.kmail_token, &config.mail_hosting_id, &config.mailbox_name, &server.url()));
     let list = api.list_aliases().await.unwrap();
     assert_eq!(list, vec!["aaa", "bbb", "ccc"]);
     mock.assert();
@@ -80,8 +80,9 @@ async fn test_list_aliases() {
 #[tokio::test]
 async fn test_add_aliases() {
     let mut server = Server::new_async().await;
-    let mock = server.mock("POST", "/1/mail_hostings/mail_id/mailboxes/mailbox_name/aliases")
-                        .with_body(r#"
+    let mock = server.mock("POST", "/1/mail_hostings/mock_mail_hosting_id/mailboxes/mock_name/aliases")
+                     .match_header(reqwest::header::AUTHORIZATION, "Bearer 123mock_kmail_token")
+                     .with_body(r#"
 
 {
 "result":"success",
@@ -90,11 +91,10 @@ async fn test_add_aliases() {
 "#)
                         .create_async()
                         .await;
-    // TODO: Check that auth token is provided in the request
-
-    let api = Arc::new(KMailApi::new("token", "mail_id", "mailbox_name", &server.url()));
 
     let config = mock_config();
+    let api = Arc::new(KMailApi::new(&config.kmail_token, &config.mail_hosting_id, &config.mailbox_name, &server.url()));
+
     let bot = MockBot::new(MockMessageText::new().text("/add"), schema());
     let probe_email_args = EmailSender::new_args_observer();
     let sender = EmailSender::new_mock(Ok(()), probe_email_args.clone());
@@ -110,3 +110,24 @@ async fn test_add_aliases() {
     assert_eq!(probe_email_args.lock().await.description, "test description");
     assert_eq!(probe_email_args.lock().await.alias_name, "added-alias-name");
 }
+
+// TODO: test each action:
+// - [/] add
+//   - [X] success path
+//   - [ ] no response
+//   - [ ] unexpected response
+//   - [ ] error response
+//   - [ ] invalid alias
+//   - [ ] existing alias?
+// - [ ] remove
+//   - [ ] success path
+//   - [ ] no response
+//   - [ ] unexpected response
+//   - [ ] error response
+//   - [ ] invalid alias
+// - [ ] list
+//   - [ ] success path
+//   - [ ] no response
+//   - [ ] unexpected response
+//   - [ ] error response
+// - [ ] user enters incorrect command
